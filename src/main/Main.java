45f2c9daf28aa823a46ae339811d8f2da6ff7c8e
riswords <riswords@gmail.com>
2016-02-15 19:34:14 -0500

    Add comments about variable numbering differences
    Solver variables are 0-indexed, whereas problem specifications are
    typically 1-indexed.

--- src/main/Main.java
+++ src/main/Main.java
@@ -30,6 +30,13 @@ public class Main {
         }
     }
     
+    /**
+     * Important note: for the solver, it's easier to index variables from 0, however, problem descriptions typically
+     * start with variable 1 and use 0 as an end of line delimiter. To compensate for this discrepancy, the parser
+     * subtracts 1 from the literal read in from the file so that the solver can use 0-indexed variables.
+     *
+     * This discrepancy is also accounted for when outputting certificates of satisfiability.
+     */
     private static void outputCertificate(BoolVec model) {
         System.out.println("s SATISFIABLE");
         for(int i=0; i<model.size(); ++i) {
